c45f8e47b93fe1a942bf718ff2636a391f9bd184
Anthony <anthony.artavia20@gmail.com>
2025-07-09 14:17:04 -0600

    Correción de los nombres de los Roles, estaban mal escritos

--- BotCoCChangeRolName.py
+++ BotCoCChangeRolName.py
@@ -1,1 +1,42 @@
 import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.members = True #Para poder detectar los miembros en el server
+intents.message_content = True # PAra poder leer los comandos como los de cambio de nombre o Rol !name
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+TOKEN = "MTM5MjU4MzU2ODg3Mzc1NDc5NA.GGielr.c4lqtR1B_5_qTzRYb4MRZHeEFC9qsEUeuWv6_U"
+ROL_VERIFICADO = "Nuevo Miembro" #Este rol es el que creé como forma de identificar aquellos que ya se cambiaron el apodo y tienen acceso al resto de canales, no solo el de bienvenida.
+ROL_PENDIENTE = "Sin Verificar(PendienteDeNombre)" #Como su nombre dice, este es para cuando la persona reciente entra al server y no se ha cambiado el nombre, osea pendiente de asignar rol.
+
+@bot.event
+async def on_ready():
+  print(f"Own Bot {bot.user.name} is ready and online!")
+
+@bot.command()
+async def nombre(ctx, *, nickname):
+  Channelmember = ctx.author
+  try:
+    #Cambio de apodo:
+    await Channelmember.edit(nick=nickname)
+
+    #Quitar el rol de "Sin verificar(...) para poder luego asignar el bueno"
+    rol_pendiente = discord.utils.get(ctx.guild.roles, name=ROL_PENDIENTE)
+    if rol_pendiente in Channelmember.roles:
+      await Channelmember.remove_roles(rol_pendiente)
+
+    #Dar el rol de verificado, "Nuevo Miembro":
+    rol_nuevo = discord.utils.get(ctx.guild.roles, name=ROL_VERIFICADO)
+    if rol_nuevo not in Channelmember.roles:
+      await Channelmember.add_roles(rol_nuevo)
+
+    await ctx.send(f"✅ ¡Nombre actualizado a **{nickname}** y acceso concedido! Recuerda leer el canal de reglas-del-clan")
+
+  except discord.Forbidden:
+    await ctx.send("No tengo permisos para cambiar tu apodo o rol o etc, admin revisa esto!")
+  except Exception as delta:
+    await ctx.send(f"Ocurrió el siguiente error: {delta}")
+
+bot.run(TOKEN)
